c9d7c51b58c17cffcb9356151b6aa99665a653a5
Leaf <57141624+leafayon@users.noreply.github.com>
2026-03-11 16:06:54 +0100

    refactor(config): .env file loading separation
    Signed-off-by: Leaf <57141624+leafayon@users.noreply.github.com>

--- go.mod
+++ go.mod
@@ -13,5 +13,5 @@ require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/mattn/go-sqlite3 v1.14.22 // indirect
-	golang.org/x/text v0.34.0 // indirect
+	golang.org/x/text v0.35.0 // indirect
 )
--- internal/infrastructure/config/config.go
+++ internal/infrastructure/config/config.go
@@ -3,13 +3,16 @@ package config
 import (
 	"github.com/caarlos0/env/v11"
 	"github.com/joho/godotenv"
-
 	"github.com/leafayon/musicalorg/internal/models"
 )
 
-func Load(envPath string) (models.Config, error) {
-	_ = godotenv.Load(envPath)
+func LoadEnv(path string) error {
+	err := godotenv.Load(path)
 
+	return err
+}
+
+func Parse() (models.Config, error) {
 	config, err := env.ParseAsWithOptions[models.Config](env.Options{
 		Prefix: "MUSICALORG_",
 	})
